c2fecbe4f010e087a67785d73f52e3713a5a075a
sasamuku <sasazawa3330@gmail.com>
2024-03-16 23:09:11 +0900

    write readme

--- spec/rspec_tree_spec.rb
+++ spec/rspec_tree_spec.rb
@@ -1,11 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe RspecTree do
-  it "has a version number" do
-    expect(RspecTree::VERSION).not_to be nil
+
+  context "when the version is 0.1.0" do
+    it "has a version number" do
+      expect(RspecTree::VERSION).not_to be nil
+    end
+
+    it "does something useful" do
+      expect(false).to eq(true)
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "when the version is 0.1.1" do
+    it "has a version number" do
+      expect(RspecTree::VERSION).not_to be nil
+    end
+
+    it "does something useful" do
+      expect(false).to eq(true)
+    end
   end
 end
